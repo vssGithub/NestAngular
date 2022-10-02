28a42d6766400aa1feb4575c715212c49f65bd84
vsm <vsm.upskilling@gmail.com>
2022-10-02 15:39:46 +0200

    - export UserService from user.module - import UserModule in auth.module - added create to save data in repository - inject dependency in auth.controller

--- nest-admin/src/auth/auth.controller.ts
+++ nest-admin/src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 
 @Controller()
 export class AuthController {
+
+    constructor(private userService: UserService) {}
     
     @Post('register')
     async register(@Body() body) {
-        return body;
+        return await this.userService.create(body);
     }
     
 }
