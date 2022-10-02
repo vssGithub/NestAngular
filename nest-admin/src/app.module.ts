8f998da1ae5a42230b9ee87ffd3263b491a9f234
vsm <vsm.upskilling@gmail.com>
2022-10-02 14:47:22 +0200

    added auth controller to create user

--- nest-admin/src/app.module.ts
+++ nest-admin/src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,7 +17,8 @@ import { UserModule } from './user/user.module';
       database: 'admin',
       autoLoadEntities: true,
       synchronize: true,
-    })
+    }),
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ nest-admin/src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+@Controller()
+export class AuthController {
+    
+    @Post('register')
+    async register(@Body() body) {
+        return body;
+    }
+    
+}
