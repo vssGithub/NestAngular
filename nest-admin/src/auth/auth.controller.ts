4116adeabfd114a05f0dc9a1a14a68c9477763dd
vsm <vsm.upskilling@gmail.com>
2022-10-02 16:39:44 +0200

    adding validations for input fields

--- nest-admin/src/auth/auth.controller.ts
+++ nest-admin/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 import { UserService } from 'src/user/user.service';
+import { RegisterDto } from './models/register.dto';
 
 @Controller()
 export class AuthController {
@@ -8,7 +9,11 @@ export class AuthController {
     constructor(private userService: UserService) {}
     
     @Post('register')
-    async register(@Body() body) {
+    async register(@Body() body: RegisterDto) {
+        if (body.password !== body.password_confirm) {
+            throw new BadRequestException('Passwords do not match');
+        }
+
         const hashedPassword = await bcrypt.hash(body.password, 12);
         return await this.userService.create({
             first_name: body.first_name,
