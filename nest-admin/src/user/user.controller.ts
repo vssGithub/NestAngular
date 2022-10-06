6861a5edbbe036c124e2aef539e17ab3b5488cb6
vsm <vsm.upskilling@gmail.com>
2022-10-06 18:56:04 +0200

    - added endpoint to create user - added endpoint to get user by id - added user dto

--- nest-admin/src/user/user.controller.ts
+++ nest-admin/src/user/user.controller.ts
@@ -1,13 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Get, Param, Post, UseGuards, UseInterceptors } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { UserCreateDto } from './models/user-create.dto';
 import { User } from './models/user.entity';
 import { UserService } from './user.service';
 
+@UseInterceptors(ClassSerializerInterceptor)
+@UseGuards(AuthGuard)
 @Controller('users')
 export class UserController {
-    constructor(private userService: UserService) {}
+    constructor(private userService: UserService) {
+        
+    }
 
     @Get()
     async all(): Promise<User[]> {
         return await this.userService.all();
+    }
+
+    @Post()
+    async create(@Body() body: UserCreateDto): Promise<User> {
+        const password = await bcrypt.hash('1234', 12);
+        
+        return this.userService.create({
+            first_name: body.first_name,
+            last_name: body.last_name,
+            email: body.email,
+            password
+
+        });
+    }
+
+    @Get(':id')
+    async get(@Param('id') id: number) {
+        return this.userService.findOne({where: {id: id}});
     }
 }
