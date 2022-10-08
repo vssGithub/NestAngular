9564cce5019a53dbffa13d0adc541c6a936179fa
vsm <vsm.upskilling@gmail.com>
2022-10-08 22:32:55 +0200

    rewiring permission service

--- nest-admin/src/permission/permission.service.ts
+++ nest-admin/src/permission/permission.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AbstractService } from 'src/common/abstract.service';
 import { Repository } from 'typeorm';
 import { Permission } from './permission.entity';
 
 @Injectable()
-export class PermissionService {
+export class PermissionService extends AbstractService {
     constructor(
         @InjectRepository(Permission) private readonly permissionRepository: Repository<Permission>
     ) {
-
+        super(permissionRepository);
     }
 
-    async all(): Promise<Permission[]> {
-        return await this.permissionRepository.find();
-    }
 }
