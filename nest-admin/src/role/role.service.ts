61349c798423c0085f1309f0bc4c1a2e70fe7746
vsm <vsm.upskilling@gmail.com>
2022-10-08 22:30:35 +0200

    moved duplicated code from role.service & user.service to newly created abstract service added interface

--- nest-admin/src/role/role.service.ts
+++ nest-admin/src/role/role.service.ts
@@ -1,33 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AbstractService } from 'src/common/abstract.service';
 import { Repository } from 'typeorm';
 import { Role } from './role.entity';
 
 @Injectable()
-export class RoleService {
+export class RoleService extends AbstractService {
     constructor(
         @InjectRepository(Role) private readonly roleRepository: Repository<Role>
-    ) {}
-
-    async all(): Promise<Role[]> {
-        return this.roleRepository.find();
-    }
-
-    async create(data): Promise<Role> {
-        return await this.roleRepository.save(data);
-    }
-
-    async findOne(condition): Promise<Role> {
-        //console.log(JSON.stringify(condition, null, 2));
-
-        return await this.roleRepository.findOne(condition);
-    }
-
-    async update(id: number, data): Promise<any> {
-        return await this.roleRepository.update(id, data);
-    }
-
-    async delete(id: number): Promise<any> {
-        return await this.roleRepository.delete(id);
+    ) {
+        super(roleRepository);
     }
 }
--- nest-admin/src/user/user.service.ts
+++ nest-admin/src/user/user.service.ts
@@ -1,49 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AbstractService } from 'src/common/abstract.service';
+import { PaginatedResult } from 'src/common/paginated-result.interface';
 import { Repository } from 'typeorm';
 import { User } from './models/user.entity';
 
 @Injectable()
-export class UserService {
+export class UserService  extends AbstractService {
     constructor(
         @InjectRepository(User) private readonly userRepository: Repository<User>
-    ) {}
-
-    async all(): Promise<User[]> {
-        return await this.userRepository.find();
+    ) {
+        super(userRepository);
     }
 
-    async paginate(page: number = 1): Promise<any> {
-        const take = 15;
-
-        const [users, total] = await this.userRepository.findAndCount({
-            take,
-            skip: (page - 1) * take
-        });
+    async paginate(page: number = 1, relations = []): Promise<PaginatedResult> {
+        const {data, meta} = await super.paginate(page, relations);
 
         return {
-            data: users,
-            meta: {
-                total,
-                page,
-                last_page: Math.ceil(total / take)
-            }
+            data: data.map((user) => {
+                const {password, ...data} = user;
+                return data;
+            }),
+            meta: meta
         }
     }
-
-    async create(data): Promise<User> {
-        return await this.userRepository.save(data);
-    }
-
-    async findOne(condition): Promise<User> {
-        return await this.userRepository.findOne(condition);
-    }
-
-    async update(id: number, data): Promise<any> {
-        return await this.userRepository.update(id, data);
-    }
-
-    async delete(id: number): Promise<any> {
-        return await this.userRepository.delete(id);
-    }
+    
 }
